cd64dc07ada1e2da6416e7c51f01c0ac3f9dcbec
zaim-br24 <learnning.zabr@gmail.com>
2022-02-09 13:56:26 +0100

    using md5() method and and mongoose encrypt method and best solution the HASH method with Salting

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5')
 
 const app = express();
 app.use(express.static('public'));
@@ -22,7 +25,7 @@ const userSchema = new mongoose.Schema({
 //:::::::::::: Encrypt Only the password
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
+// userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
@@ -42,20 +45,25 @@ app.get('/register', (req, res)=>{
 
 
 
-app.post('/register', (req, res)=>{
-    const newUser = new User(
-        {
-        email: req.body.username,
-        password: req.body.password
-        })
 
-        newUser.save((err)=>{
-            if(err){
-                console.log(err)
-            }else{
-                res.render('secrets')
-            }
-        })
+app.post('/register', (req, res)=>{
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User(
+            {
+            email: req.body.username,
+            password: hash
+            })
+    
+            newUser.save((err)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    res.render('secrets')
+                }
+            })
+    });
+ 
         
 })
 
@@ -68,9 +76,16 @@ app.post('/login', (req , res)=> {
         if(err){
             console.log(err)
         }else if(foundUser){
-            if(foundUser.password === userPassword){
+            bcrypt.compare(userPassword, foundUser.password, function(err, result) {
+               if(result){
                 res.render('secrets')
-            }
+               }else{
+                   //here we can render password not match popup
+                   console.log('please your password is not match.')
+               }
+            });
+                
+            
         }else{
             res.redirect('/register')
         }
